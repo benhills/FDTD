8d88fd07920ad45febc96183891a9c2fa119b131
benhills <benjaminhhills@gmail.com>
2017-12-13 18:28:50 -0800

    Clean Up Model Scripts
    After plotting all the figures for the paper, I took out a bunch
    of the crap in the scripts so that it looks cleaner and will
    be easier to come back to.

--- 1d_model.py
+++ 1d_model.py
@@ -15,73 +15,70 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.sparse as sp
 import math
-import matplotlib.animation as animation
 
 ############################################################
 ### Setup ###
 
 # Constants
-c0 = 3e8        # Speed of Light m/s
-e0 = 8.85e-12   # free space permittivity 1/m
-u0 = 1.26e-6    # free space permeability 1/m
-fmax   = 30e6    # Source Frequency 1/s
-
-# bed properties
-erice = 3.2          # relative permittivity of ice
-erdebris = 2.         # relative permittivity of debris laden ice
-erbed = 12.         # relative permittivity of bed
+c0   = 3e8                                      # Speed of Light m/s
+e0   = 8.85e-12                                 # free space permittivity 1/m
+u0   = 1.26e-6                                  # free space permeability 1/m
+fmax = 30e6                                     # Source Frequency 1/s
+erice = 3.2                                     # relative permittivity of ice
+erdebris = 2.                                   # relative permittivity of debris laden ice
+erbed = 12.                                     # relative permittivity of bed
 
 ### Grid Parameters ###
 ermax = max([erice, erbed])                     # maximum relative permittivity
 nmax  = np.sqrt(ermax)                          # maximum refractive index
 NLAM  = 10                                      # grid resolution, resolve nmax with 10pts
 lam0  = c0/fmax                                 # min wavelength in simulation
-dz = lam0/nmax/NLAM          # step size in x/z-direction
+dz = lam0/nmax/NLAM                             # step size in x/z-direction
 zmax = 100.
-Z   = np.arange(-zmax/2.,1.5*zmax,dz)  # X-distance and Z-depth arrays for domain
-Nz = len(Z)                                 # number of x/z points in grid
-nbed_1 = np.argmin(abs(Z-0.8*zmax))                              # bed start location
-nbed_2 = -1                              # bed start location
+Z   = np.arange(0.,zmax,dz)                     # X-distance and Z-depth arrays for domain
+Nz = len(Z)                                     # number of x/z points in grid
 
 # Initialize material constants
-Er = erice*np.ones(Nz)              # relative permittivity
-Ur = (1/erice)*np.ones(Nz)                    # relative permeability
+Er = erice*np.ones(Nz)                          # relative permittivity
+Ur = (1/erice)*np.ones(Nz)                      # relative permeability
+
 # change the permittivity for some random layers that have debris and for the bed
+nbed_1 = np.argmin(abs(Z-0.8*zmax))             # bed start location
+nbed_2 = -1                                     # bed start location
 rand_ind  = np.where(np.random.rand(Nz)>0.8)
-Er[rand_ind] = erdebris       # relative permittivity in the slab
-Er[nbed_1:nbed_2] = erbed                   # relative permittivity in the bed
+Er[rand_ind] = erdebris                         # relative permittivity in the slab
+Er[nbed_1:nbed_2] = erbed                       # relative permittivity in the bed
 
 # Time Domain
-nbc   = np.sqrt(Ur[0]*Er[0])        # refractive index at boundary
-dt    = nbc*dz/(2*c0)               # time step
-tau   = 0.5/fmax                    # duration of Gaussian source
-t0    = 5.*tau                      # initial time, offset of Gaussian source
-tprop = nmax*Nz*dz/c0               # time for wave accross grid
-t_f     = 2.*t0 + 0.3*tprop          # total simulation time
-steps = math.floor((t_f/dt))           # number of time steps
-t     = np.arange(0,steps)*dt       # update simulation time
+nbc   = np.sqrt(Ur[0]*Er[0])                    # refractive index at boundary
+dt    = nbc*dz/(2*c0)                           # time step
+tau   = 0.5/fmax                                # duration of Gaussian source
+t0    = 5.*tau                                  # initial time, offset of Gaussian source
+tprop = nmax*Nz*dz/c0                           # time for wave accross grid
+t_f   = 2.*t0 + 0.3*tprop                       # total simulation time
+steps = math.floor((t_f/dt))                    # number of time steps
+t     = np.arange(0,steps)*dt                   # update simulation time
 
 # Source
-#nz_src = math.ceil(Nz/2.)                   # Index of Source Location (centered)
-nz_src = np.argmin(abs(Z-0.))               # Source at the start
-Esrc   = np.exp(-((t-t0)/tau)**2.)          # Electricity source, Gaussian
+nz_src = np.argmin(abs(Z-zmax/2.))              # Index of Source Location (centered)
+Esrc   = np.exp(-((t-t0)/tau)**2.)              # Electricity source, Gaussian
 
 # Initialize FDTD parametrs
-mEy = (c0*dt)/Er    # Electricity multiplication parameter
-mHx = (c0*dt)/Ur    # Magnetism multiplication parameter
+mEy = (c0*dt)/Er                                # Electricity multiplication parameter
+mHx = (c0*dt)/Ur                                # Magnetism multiplication parameter
 # Initialize fields to zero
-Ey = np.zeros(Nz)   # Electric Field
-Hx = np.zeros(Nz)   # Magnetic Field
+Ey = np.zeros(Nz)                               # Electric Field
+Hx = np.zeros(Nz)                               # Magnetic Field
 
 ############################################################
 ### Matrices ###
 
 # Define transformation matrices for forward difference
-A = sp.lil_matrix((Nz,Nz))          # Sparse Matrix for Hx update
-A.setdiag(-1.*np.ones(Nz),k=0)      # Matrix diagonal to -1
-A.setdiag(np.ones(Nz-1),k=1)        # Matrix off-diagonal to 1
+A = sp.lil_matrix((Nz,Nz))                      # Sparse Matrix for Hx update
+A.setdiag(-1.*np.ones(Nz),k=0)                  # Matrix diagonal to -1
+A.setdiag(np.ones(Nz-1),k=1)                    # Matrix off-diagonal to 1
 
-B = sp.lil_matrix((Nz,Nz))          # Sparse Matrix for Ey update
+B = sp.lil_matrix((Nz,Nz))                      # Sparse Matrix for Ey update
 B.setdiag(np.ones(Nz),k=0)
 B.setdiag(-1.*np.ones(Nz-1),k=-1)
 
@@ -97,43 +94,23 @@ E1,E2,E3 = 0,0,0
 ############################################################
 ### Figure ###
 
-"""
 fig = plt.figure(figsize=(6,6))
 ax1 = plt.subplot(111)
-ax1.set_xlim(-1.5,1.5)
-ax1.set_ylim(1.,0)
-plt.ylabel('Distance')
-plt.xlabel('Normalized EM Field')
-plt.ion()
+ax1.set_ylim(-1.5,1.5)
+ax1.set_xlim(0,zmax)
+plt.xlabel('Distance')
+plt.ylabel('Normalized EM Field')
 time_text = ax1.text(0.5,1.1,'',ha='center',transform=ax1.transAxes)
 
-ax2 = plt.subplot(122)
-plt.xlim(0,dt*steps)
-plt.ylim(-1,1)
-plt.xlabel('seconds')
-plt.ylabel('E-return')
-
-# fill the bed and debris locations
-for i in rand_ind[0]:
-    ax1.axhline(Z[i],c='k',alpha=0.2)
-ax1.fill_between(np.linspace(-5,5,10),Z[nbed_1],Z[nbed_2],color='k',zorder=0)
 # plot Electric and Magnetic field
 H_line, = ax1.plot([],[],'b',zorder=1)
 E_line, = ax1.plot([],[],'r',zorder=2)
 
-# plot the power output through time
-#P_line, = ax2.plot([],[],'k')
-
-#plt.tight_layout()
-"""
+plt.ion()
 
 ############################################################
+
 ### Algorithm ###
-
-E_out = np.empty((steps,len(Ey)))
-H_out = np.empty((steps,len(Hx)))
-P_out = [[],[]]
-
 for t_i in np.arange(steps):
     # Update Magnetic Field
     Hx += (mHx/dz)*(A*Ey)
@@ -154,115 +131,11 @@ for t_i in np.arange(steps):
     # Apply the source
     Ey[nz_src] = Ey[nz_src] + Esrc[t_i]
 
-    # save the fields to an array
-    #E_out[t_i] = Ey
-    #H_out[t_i] = Hx/erice
-
-    # Save the E-field at the top to an array
-    P_out[0].append(dt*t_i)
-    P_out[1].append(Ey[nz_src])
-    """
+    # Update the Plot
     E_line.set_ydata(Z)
     E_line.set_xdata(Ey)
     H_line.set_ydata(Z+0.5*dz)
     H_line.set_xdata(Hx/erice)
-    #P_line.set_xdata(P_out[0])
-    #P_line.set_ydata(P_out[1])
-    #time_text.set_text('Time Step = %0.0f of %0.0f' % (t_i,steps))
+    time_text.set_text('Time Step = %0.0f of %0.0f' % (t_i,steps))
     plt.pause(0.00001)
-    """
-    print t_i, steps
 
-    if t_i == int(steps/10):
-        fig = plt.figure(figsize=(12,9))
-
-        import matplotlib.gridspec as gridspec
-
-        gs = gridspec.GridSpec(4,4)
-
-        ax1 = plt.subplot(gs[:3,0])
-        ax1.set_xlim(-1.5,1.5)
-        ax1.set_ylim(zmax,0.)
-        plt.ylabel('Depth')
-        plt.xlabel('Normalized EM Field')
-        plt.plot(Hx/erice,Z+0.5*dz,'b')
-        plt.plot(Ey,Z,'r')
-        for zi in rand_ind[0]:
-            ax1.axhline(Z[zi],c='k',alpha=0.2)
-        ax1.fill_between(np.linspace(-5,5,10),Z[nbed_1],Z[nbed_2],color='k',zorder=0)
-        ax1.text(0.1,0.95,'(a)',size='large',weight='bold',ha='center',transform=ax1.transAxes)
-        plt.title('%0.2E sec'%t[t_i])
-
-    elif t_i == int(3*steps/10):
-        ax2 = plt.subplot(gs[:3,1])
-        ax2.set_xlim(-1.5,1.5)
-        ax2.set_ylim(zmax,0.)
-        ax2.tick_params(which='both',labelleft='off')
-        plt.plot(Hx/erice,Z+0.5*dz,'b')
-        plt.plot(Ey,Z,'r')
-        for zi in rand_ind[0]:
-            ax2.axhline(Z[zi],c='k',alpha=0.2)
-        ax2.fill_between(np.linspace(-5,5,10),Z[nbed_1],Z[nbed_2],color='k',zorder=0)
-        ax2.text(0.1,0.95,'(b)',size='large',weight='bold',ha='center',transform=ax2.transAxes)
-        plt.title('%0.2E sec'%t[t_i])
-
-    elif t_i == int(5*steps/10):
-        ax3 = plt.subplot(gs[:3,2])
-        ax3.set_xlim(-1.5,1.5)
-        ax3.set_ylim(zmax,0.)
-        ax3.tick_params(which='both',labelleft='off')
-        plt.plot(Hx/erice,Z+0.5*dz,'b')
-        plt.plot(Ey,Z,'r')
-        for zi in rand_ind[0]:
-            ax3.axhline(Z[zi],c='k',alpha=0.2)
-        ax3.fill_between(np.linspace(-5,5,10),Z[nbed_1],Z[nbed_2],color='k',zorder=0)
-        ax3.text(0.1,0.95,'(c)',size='large',weight='bold',ha='center',transform=ax3.transAxes)
-        plt.title('%0.2E sec'%t[t_i])
-
-    elif t_i == int(7*steps/10):
-        ax4 = plt.subplot(gs[:3,3])
-        ax4.set_xlim(-1.5,1.5)
-        ax4.set_ylim(zmax,0.)
-        ax4.tick_params(which='both',labelleft='off')
-        plt.plot(Hx/erice,Z+0.5*dz,'b')
-        plt.plot(Ey,Z,'r')
-        for zi in rand_ind[0]:
-            ax4.axhline(Z[zi],c='k',alpha=0.2)
-        ax4.fill_between(np.linspace(-5,5,10),Z[nbed_1],Z[nbed_2],color='k',zorder=0)
-        ax4.text(0.1,0.95,'(d)',size='large',weight='bold',ha='center',transform=ax4.transAxes)
-        plt.title('%0.2E sec'%t[t_i])
-
-ax5 = plt.subplot(gs[3,:])
-ax5.set_ylim(-1.,1.)
-plt.plot(P_out[0],P_out[1])
-ax5.text(0.04,0.85,'(e)',size='large',weight='bold',ha='center',transform=ax5.transAxes)
-plt.ylabel('E-return')
-plt.xlabel('seconds')
-
-plt.tight_layout()
-
-plt.savefig('IceSimulation.png',dpi=300)
-
-############################################################
-"""
-def init():
-    E_line.set_data([],[])
-    H_line.set_data([],[])
-    P_line.set_data([],[])
-    return E_line, H_line, P_line,
-
-def animate(i):
-    E_line.set_ydata(Z)
-    E_line.set_xdata(E_out[i])
-    H_line.set_ydata(Z+0.5*dz)
-    H_line.set_xdata(H_out[i])
-    P_line.set_xdata(P_out[0][:i])
-    P_line.set_ydata(P_out[1][:i])
-    time_text.set_text('Time Step = %0.0f of %0.0f' % (i,steps))
-    return E_line, H_line, P_line, time_text,
-
-ani = animation.FuncAnimation(fig,animate,init_func=init,frames=np.arange(0,steps,2),interval=20,blit=True)
-
-# Save the animation
-ani.save('WithLayers.mp4',writer="ffmpeg")
-"""
--- 2d_model.py
+++ 2d_model.py
@@ -14,85 +14,80 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.sparse as sp
 import math
-import matplotlib.animation as animation
 
 ############################################################
 ### Setup ###
 
 # Constants
-c0 = 3e8        # Speed of Light m/s
-e0 = 8.85e-12   # free space permittivity 1/m
-u0 = 1.26e-6    # free space permeability 1/m
-fmax   = 5e9    # Source Frequency 1/s
-
-# bed properties
-erice = 1.          # relative permittivity of ice
-erbed = 12.         # relative permittivity of bed
+c0    = 3e8                                     # Speed of Light m/s
+e0    = 8.85e-12                                # free space permittivity 1/m
+u0    = 1.26e-6                                 # free space permeability 1/m
+fmax  = 30e6                                    # Source Frequency 1/s
+erice = 3.2                                     # relative permittivity of ice
+erbed = 12                                      # relative permittivity of bed
 
 ### Grid Parameters ###
 ermax = max([erice, erbed])                     # maximum relative permittivity
 nmax  = np.sqrt(ermax)                          # maximum refractive index
 NLAM  = 10                                      # grid resolution, resolve nmax with 10pts
 lam0  = c0/fmax                                 # min wavelength in simulation
-dx,dy = 2*lam0/nmax/NLAM, 2*lam0/nmax/NLAM          # step size in x/z-direction
+dx,dy = 2*lam0/nmax/NLAM, 2*lam0/nmax/NLAM      # step size in x/z-direction
 X,Y   = np.arange(0,1.,dx), np.arange(0,1.,dy)  # X-distance and Z-depth arrays for domain
-Nx,Ny = len(X),len(Y)                                 # number of x/z points in grid
+Nx,Ny = len(X),len(Y)                           # number of x/z points in grid
 
 # Initialize material constants
-N = Nx*Ny
-epsz = erice*np.ones(N)              # relative permittivity
-mux = np.ones(N)                    # relative permeability
-muy = np.ones(N)                    # relative permeability
-
-#muy[Nx*120:Nx*140] = 10.        # relative permeability in the anisotropic zone
+N    = Nx*Ny
+epsz = erice*np.ones(N)                         # relative permittivity in the z direction
+mux  = np.ones(N)                               # relative permeability in the x direction
+muy  = np.ones(N)                               # relative permeability in the y direction
 
 # Time Domain
-nbc   = np.sqrt(mux[0]*epsz[0])        # refractive index at boundary
-dt    = nbc*dy/(2*c0)               # time step
-tau   = 0.2/fmax                    # duration of Gaussian source
-t0    = 2.*tau                      # initial time, offset of Gaussian source
-tprop = nmax*Ny*dy/c0               # time for wave accross grid
-t_f     = 2.*t0 + 3.*tprop          # total simulation time
-steps = 500#math.ceil(t_f/dt)           # number of time steps
-t     = np.arange(0,steps)*dt       # update simulation time
+nbc   = np.sqrt(mux[0]*epsz[0])                 # refractive index at boundary
+dt    = nbc*dy/(2*c0)                           # time step
+tau   = 0.2/fmax                                # duration of Gaussian source
+t0    = 2.*tau                                  # initial time, offset of Gaussian source
+tprop = nmax*Ny*dy/c0                           # time for wave accross grid
+t_f   = 2.*t0 + 3.*tprop                        # total simulation time
+steps = math.ceil(t_f/dt)                       # number of time steps
+t     = np.arange(0,steps)*dt                   # update simulation time
 
 # Source
-nx_src = math.ceil(Nx/4.)                   # x Index of Source Location
-ny_src = math.ceil(Ny/4.)                   # y Index of Source Location
-n_src = int(ny_src*Nx+nx_src)
-Esrc   = 40*np.exp(-((t-t0)/tau)**2.)          # Electricity source, Gaussian
+nx_src = math.ceil(Nx/4.)                       # x Index of Source Location
+ny_src = math.ceil(Ny/4.)                       # y Index of Source Location
+n_src  = int(ny_src*Nx+nx_src)                  # Source location in vector
+Esrc   = 40*np.exp(-((t-t0)/tau)**2.)           # Electricity source, Gaussian
 
 # Initialize FDTD parametrs
-mEz = (c0*dt)/epsz    # Electricity multiplication parameter
-mHx = (c0*dt)/mux    # Magnetism multiplication parameter
-mHy = (c0*dt)/muy    # Magnetism multiplication parameter
+mEz = (c0*dt)/epsz                              # Electricity multiplication parameter
+mHx = (c0*dt)/mux                               # Magnetism multiplication parameter
+mHy = (c0*dt)/muy                               # Magnetism multiplication parameter
 # Initialize fields to zero
-Ez = np.zeros((N))   # Electric Field
-Hx = np.zeros((N))  # Magnetic Field in x direction
-Hy = np.zeros((N))  # Magnetic Field in y direction
+Ez = np.zeros((N))                              # Electric Field in z direction
+Hx = np.zeros((N))                              # Magnetic Field in x direction
+Hy = np.zeros((N))                              # Magnetic Field in y direction
 
 ############################################################
 ### Matrices ###
 
 # Define transformation matrices for forward difference
-Ea = sp.lil_matrix((N,N))               # Sparse Matrix for Hx update
-Ea.setdiag(-1.*np.ones(N),k=0)          # Matrix diagonal to -1 for the node itself
-Ea.setdiag(np.ones(N-Nx),k=Nx)          # Matrix off-diagonal to 1 for the node in the y-direction
+Ea = sp.lil_matrix((N,N))                       # Sparse Matrix for Hx update
+Ea.setdiag(-1.*np.ones(N),k=0)                  # Matrix diagonal to -1 for the node itself
+Ea.setdiag(np.ones(N-Nx),k=Nx)                  # Matrix off-diagonal to 1 for the node in the y-direction
 Ea/=dy
 
-Eb = sp.lil_matrix((N,N))               # Sparse Matrix for Hy update
-Eb.setdiag(-1.*np.ones(N),k=0)          # Matrix diagonal to -1 for the node itself
-Eb.setdiag(np.ones(N-1),k=1)          # Matrix off-diagonal to 1 for the node in the y-direction
+Eb = sp.lil_matrix((N,N))                       # Sparse Matrix for Hy update
+Eb.setdiag(-1.*np.ones(N),k=0)                  # Matrix diagonal to -1 for the node itself
+Eb.setdiag(np.ones(N-1),k=1)                    # Matrix off-diagonal to 1 for the node in the x-direction
 Eb/=dx
 
-Ha = sp.lil_matrix((N,N))               # Sparse Matrix for Ey update
+Ha = sp.lil_matrix((N,N))                       # Sparse Matrix to be multiplied by Hx
 Ha.setdiag(np.ones(N),k=0)
-Ha.setdiag(-1.*np.ones(N-Nx),k=-Nx)       # Matrix off-diagonal to 1 for the node in the x-direction
+Ha.setdiag(-1.*np.ones(N-Nx),k=-Nx)             # Matrix off-diagonal to -1 for the node in the y-direction
 Ha/=dy
 
-Hb = sp.lil_matrix((N,N))               # Sparse Matrix for Ey update
+Hb = sp.lil_matrix((N,N))                       # Sparse Matrix to be multiplied by Hy
 Hb.setdiag(np.ones(N),k=0)
-Hb.setdiag(-1.*np.ones(N-1),k=-1)       # Matrix off-diagonal to 1 for the node in the x-direction
+Hb.setdiag(-1.*np.ones(N-1),k=-1)               # Matrix off-diagonal to -1 for the node in the x-direction
 Hb/=dx
 
 # Dirichlet BCs
@@ -101,112 +96,31 @@ Eb[np.arange(Ny)*Nx-1,:] = 0
 Ha[:Nx+1,:] = 0
 Hb[np.arange(Ny)*Nx,:] = 0
 
-# Perfectly absorbing BC
-PABC = False
-H1,H2,H3 = 0,0,0
-E1,E2,E3 = 0,0,0
-
 ############################################################
 ### Figure ###
 
 fig = plt.figure(figsize=(12,9))
-"""
 ax = plt.subplot()
 
 plt.ion()
 im = plt.imshow(Ez.reshape(Nx,Ny),vmin=-1.,vmax=1.,cmap='RdYlBu')
 plt.colorbar()
 time_text = ax.text(0.5,1.05,'',ha='center',transform=ax.transAxes)
-"""
-
-Xs,Ys = np.meshgrid(X,Y)
 
 ############################################################
 
 ### Algorithm ###
-
-E_out = np.empty((steps,len(Ez)))
-
 for t_i in np.arange(steps):
 
     # Update Magnetic Field
     Hx += -mHx*(Ea*Ez)
     Hy += mHy*(Eb*Ez)
-    if PABC == True:
-        # Record H-field at Boundary
-        H3 = H2
-        H2 = H1
-        H1 = Hx[0]
-
     # Update Electric Field
     Ez += mEz*(Hb*Hy-Ha*Hx)
-    if PABC == True:
-        # Record E-field at Boundary
-        E3 = E2
-        E2 = E1
-        E1 = Ez[-1]
     # Apply the source
     Ez[n_src] += Esrc[t_i]
 
-    # Plot
-    #im.set_data(Ez.reshape(Nx,Ny))
-    #time_text.set_text('Time Step = %0.0f of %0.0f' % (t_i,steps))
-    #plt.pause(0.000001)
-
-    #E_out[t_i] = Ez
-
-    print t_i,steps
-
-    if t_i == int(steps/10):
-        ax1 = plt.subplot(221)
-        im = plt.pcolor(Xs,Ys,Ez.reshape(Nx,Ny),vmin=-1.,vmax=1.,cmap='RdYlBu')
-        plt.title('%0.2E sec'%t[t_i])
-        plt.xlabel('m')
-        plt.ylabel('m')
-
-    if t_i == int(3*steps/10):
-        ax2 = plt.subplot(222)
-        plt.pcolor(Xs,Ys,Ez.reshape(Nx,Ny),vmin=-1.,vmax=1.,cmap='RdYlBu')
-        plt.title('%0.2E sec'%t[t_i])
-        plt.xlabel('m')
-        plt.ylabel('m')
-
-    if t_i == int(6*steps/10):
-        ax3 = plt.subplot(223)
-        plt.pcolor(Xs,Ys,Ez.reshape(Nx,Ny),vmin=-1.,vmax=1.,cmap='RdYlBu')
-        plt.title('%0.2E sec'%t[t_i])
-        plt.xlabel('m')
-        plt.ylabel('m')
-
-    if t_i == int(10*steps/10)-1:
-        ax4 = plt.subplot(224)
-        plt.pcolor(Xs,Ys,Ez.reshape(Nx,Ny),vmin=-1.,vmax=1.,cmap='RdYlBu')
-        plt.title('%0.2E sec'%t[t_i])
-        plt.xlabel('m')
-        plt.ylabel('m')
-
-plt.tight_layout()
-
-plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9)
-cax = plt.axes([0.85 0.1, 0.05, 0.8])
-cbar = plt.colorbar(cax=cax)
-cbar.set_label('Normalized EM Field')
-
-plt.savefig('2D_figure.png',dpi=300)
-
-############################################################
-"""
-def init():
-    im.set_data([[],[]])
-    return im,
-
-def animate(i):
-    im.set_data(E_out[i].reshape(Nx,Ny))
-    time_text.set_text('Time Step = %0.0f of %0.0f' % (i,steps))
-    return im, time_text,
-
-ani = animation.FuncAnimation(fig,animate,init_func=init,frames=np.arange(0,steps,2),interval=20,blit=True)
-
-# Save the animation
-ani.save('Anisotropic.mp4',writer="ffmpeg")
-"""
+    # Update the Plot
+    im.set_data(Ez.reshape(Nx,Ny))
+    time_text.set_text('Time Step = %0.0f of %0.0f' % (t_i,steps))
+    plt.pause(0.000001)
